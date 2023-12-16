7c473f0987a7449fdb00ac9452f0fb0aa79621d6
Anirudhmadhavkulkarni9094 <Anirudhkulkarni9094@gmail.com>
2023-12-16 19:24:41 +0530

    search button in forum

--- src/Components/Forum/FORUM_CONTENT.js
+++ src/Components/Forum/FORUM_CONTENT.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import Toast from '../Toast/Toast';
 
 function ForumContent() {
     const [blogs, setBlogs] = useState([]);
@@ -8,6 +9,9 @@ function ForumContent() {
     const [newBlog, setNewBlog] = useState({ title: '', content: '', name: '', category: localStorage.getItem("category"), author: '' });
     const [selectedBlogId, setSelectedBlogId] = useState(null);
     const [AddBlog , setAddBlog] = useState(false);
+    const [showToast , setShowToast] = useState(false);
+    const [toast , setToast] = useState("");
+    const [error , setError] = useState(false);
 
     useEffect(() => {
         fetchBlogs();
@@ -36,54 +40,76 @@ function ForumContent() {
         try {
             await axios.post('https://mindwellnesspro.onrender.com/blog-posts', newBlog);
             fetchBlogs();
+            setNewBlog({ title: '', content: '' , author: '' });
+            setToast("Blog added successfully");
+            setError(false)
+            setAddBlog(false)
+            setShowToast(true);
         } catch (error) {
+            setToast("Blog Cannot be added");
+            setError(true);
             console.error('Error adding blog:', error);
         }
     };
 
+    useEffect(()=>{
+        setTimeout(()=>{
+            setShowToast(false);
+        } , 3000)
+    },[showToast])
+
     return (
         <>
+        {showToast && <Toast message={toast} error={error}/>}
         <Link to="/Forum" className='mx-10 font-bold'> &#x2190; Go back</Link>
         <div className="max-w-4xl mx-auto p-6">
             <button className="text-xl flex gap-5 items-center font-bold text-gray-500 mb-4" onClick={()=>setAddBlog(!AddBlog)}>Share your thoughts now! <img src={AddBlog ? require("../Assets/collapse.png") : require("../Assets/add.png")} className='w-5' alt=''></img> </button>
             {AddBlog && <div className="mb-4">
-                <input
-                    className="w-full rounded p-2 my-2 border"
-                    type="text"
-                    value={newBlog.title}
-                    onChange={(e) => setNewBlog({ ...newBlog, title: e.target.value })}
-                    placeholder="Enter Blog Title"
-                    />
+                <form onSubmit={handleAddBlog}>
                 <input
                     className="w-full rounded p-2 border"
                     type="text"
                     value={newBlog.author}
                     onChange={(e) => setNewBlog({ ...newBlog, author: e.target.value })}
                     placeholder="Enter Author Name"
+                    required
+                    />
+                <input
+                    className="w-full rounded p-2 my-2 border"
+                    type="text"
+                    value={newBlog.title}
+                    onChange={(e) => setNewBlog({ ...newBlog, title: e.target.value })}
+                    placeholder="Enter Blog Title"
+                    required
                     />
                 <textarea
                     className="w-full rounded p-2 border mt-2 h-56"
                     value={newBlog.content}
                     onChange={(e) => setNewBlog({ ...newBlog, content: e.target.value })}
                     placeholder="Enter Blog Content"
+                    required
                     ></textarea>
                 <button
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
-                    onClick={handleAddBlog}
-                >
+                    type = "submit"
+                    >
                     Add Blog
                 </button>
+                    </form>
             </div>}
 
             {blogs.map(blog => (
-                <div key={blog._id} className="bg-gray-100 p-4 rounded shadow mb-4">
+                <div key={blog._id} className="bg-gray-100  relative p-4 rounded shadow mb-4">
+                <p className='text-gray-400'>@{blog.author.slice(0,1).toUpperCase()}{blog.author.slice(1)}</p>
                 <h1 className="text-xl mb-2 font-bold">{blog.title.toUpperCase()}</h1>
                 <p className='mb-5'>{blog.content}</p>
+                <p className='absolute top-5 text-sm text-gray-400  right-10'>{new Date(blog.date).toLocaleString()}</p>
                 <hr className='h-4'></hr>
                 <h1 onClick={() => setSelectedBlogId(selectedBlogId === blog._id ? null : blog._id)} className='flex gap-2 items-center mt-5 font-bold text-gray-700'>
                     {selectedBlogId === blog._id ? 'Hide Comments' : 'Show Comments'} 
                     <img src={selectedBlogId === blog._id ? require("../Assets/up.png") : require("../Assets/down.png")} className='w-5' alt=''></img>
                 </h1>
+               
                 {selectedBlogId === blog._id &&
                     <div className="mt-2">
                         {/* Your comment input and display logic */}
@@ -93,6 +119,7 @@ function ForumContent() {
                             value={newComment}
                             onChange={(e) => setNewComment(e.target.value)}
                             placeholder="Add a Comment"
+                            required
                             />
                         <button
                             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded mt-2"
--- src/Components/Forum/FORUM_HOME_PAGE.js
+++ src/Components/Forum/FORUM_HOME_PAGE.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 function FORUM_HOME_PAGE() {
   // Sample channel data for different mental health conditions
+  const [filterName , setFilterName] = useState("");
   const channels = [
     { id: 1, name: 'Anxiety', description: 'Discussions about anxiety and coping mechanisms.' , category : "ANX" },
     { id: 2, name: 'Depression', description: 'Support and advice for dealing with depression.' , category : "DEP"},
@@ -17,20 +18,24 @@ function FORUM_HOME_PAGE() {
     { id: 12, name: 'Autism Spectrum', description: 'Community support and resources for autism.' , category : "AUT"},
   ];
   
+  let FilteredChannel = channels.filter(channel => {return channel.name.includes(filterName)});
 
   return (
     <>
     
     <div className="bg-gray-100 min-h-screen p-5 py-8">
       <div className="container mx-auto">
+        <div className='flex justify-around'>
         <h1 className="text-3xl font-bold mb-6">Welcome to the Mental Health Forum</h1>
+        <input onChange={(e)=>{setFilterName(e.target.value)}} placeholder='Search here' className='px-5 py-2 shadow-2xl border border-1'></input>
+        </div>
         <p className="text-lg text-gray-600 mb-8">
           This forum aims to provide a safe space for discussing various mental health conditions.
           Explore and join channels below to connect with others facing similar challenges.
         </p>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {channels.map((channel) => (
+          { FilteredChannel.length >0  ? FilteredChannel.map((channel) => (
             <div
             key={channel.id}
             className="bg-white rounded-lg p-6 shadow-md transition duration-300 ease-in-out transform hover:scale-105"
@@ -41,7 +46,19 @@ function FORUM_HOME_PAGE() {
                 Open forum
               </Link>
             </div>
-          ))}
+          )) : channels.map((channel) => (
+            <div
+            key={channel.id}
+            className="bg-white rounded-lg p-6 shadow-md transition duration-300 ease-in-out transform hover:scale-105"
+            >
+              <h2 className="text-xl font-semibold mb-2">{channel.name}</h2>
+              <p className="text-gray-600 mb-4">{channel.description}</p>
+              <Link className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none" to={`/Forum/channel`} onClick={()=>localStorage.setItem("category" , channel.category)}>
+                Open forum
+              </Link>
+            </div>
+          ))  
+          }
         </div>
       </div>
     </div>
